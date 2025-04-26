2723a2eca53e113cf78ca7d4d806ed9e585578fa
Rikko Matsumato <kuttimsx@hotmail.com>
2025-04-26 17:33:36 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+import os
+import pathlib
+
+def GetPathOfBOTN_Boolean():
+    botn_path = str("E:\\SteamLibrary\\steamapps\\common\\Breeders of the Nephelym Alpha") # Make Sure what this path is right, if not please change path :D
+    return pathlib.Path(botn_path).is_dir()
+def GetPathofBOTN():
+    botn_path = str("E:\\SteamLibrary\\steamapps\\common\\Breeders of the Nephelym Alpha")
+    return os.path.abspath(botn_path)
+
+def Main():
+    print(f"You're BOTN(Breeders of the Nephelym) Path is {GetPathofBOTN()}")
+    if(GetPathOfBOTN_Boolean() == True):
+        with open("{}".format(GetPathofBOTN() + "\\imma.degenerate"), "w") as write_file:
+            write_file.write(str(""))
+            os._exit(443)
+    else:
+        print("Please Download BOTN(Breeders of the Nephelym)!!!")
+        os._exit(43321)
+
+if __name__ == "__main__":
+    Main()
